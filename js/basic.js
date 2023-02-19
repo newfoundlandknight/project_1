658cef2a15ab04d8da5c0daf359627b141f30a30
Jack Lanphear <newfondlandknight@gmail.com>
2023-02-18 22:17:53 -0330

    modified:   js/basic.js

--- js/basic.js
+++ js/basic.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $("#burger").click(function(){
-$("#dropdown1").toggle();
+$("#dropdown1").toggle("slow");
 });
-});
+});// the toggle function using jQuery to listen to click events on the menu burger image and when triggered to toggle the 

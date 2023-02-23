b527c284cb7151b5bd6d67515ecf56eddc92b756
Jack Lanphear <newfondlandknight@gmail.com>
2023-02-22 22:51:07 -0330

    I am not a big fan

--- js/basic.js
+++ js/basic.js
@@ -3,3 +3,18 @@ $(document).ready(function(){
 $("#dropdown1").toggle("slow");
 });
 });// the toggle function using jQuery to listen to click events on the menu burger image and when triggered to toggle the 
+
+$(document).ready(function(){
+    $("#clicker_exmample").click(function(){
+
+
+    //$("#dropdown1").toggle("slow");
+    $("#template_h3").html("Somewhere in NS");
+    $("#template_date").html("Date 2023-03-28");
+    $("#template_dur").html("Duration 8 days");
+    $("#template_h4").html("Summary:");
+    $("#template_summary_details").html(" amet consectetur adipisicing elit. Non ratione, at delectus earum sapiente corrupti assumenda pariatur ipsa dolorum laborum ut minus voluptas rem. Odit ducimus consequatur et esse ipsa! here is:");
+
+});
+});// the toggle function using jQuery to listen to click events on the menu burger image and when triggered to toggle the 
+
